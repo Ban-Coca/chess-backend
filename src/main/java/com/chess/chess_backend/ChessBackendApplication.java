ad8534d592773851eaa920bb052231a85a18b38a
Van Harvey Coca <vanharvey.coca@cit.edu>
2024-12-26 18:23:29 +0800

    established connection with supabase

--- src/main/java/com/chess/chess_backend/ChessBackendApplication.java
+++ src/main/java/com/chess/chess_backend/ChessBackendApplication.java
@@ -3,11 +3,14 @@ package com.chess.chess_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ChessBackendApplication {
+import io.github.cdimascio.dotenv.Dotenv;
 
+@SpringBootApplication
+public class ChessBackendApplication  {
+	
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
 		SpringApplication.run(ChessBackendApplication.class, args);
 	}
-
 }
